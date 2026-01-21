d5c4fcf61b78f480f19bd4ce6b1a5e4736f69a62
Lei Song <pinot.sung@gmail.com>
2026-01-21 11:35:02 -0500

    Auto-knit updates for: index.Rmd, syllabus.Rmd

---
+++ scripts/students.R
@@ -0,0 +1,20 @@
+library(readxl)
+library(dplyr)
+
+load_roster <- function(fn){
+    dat <- read_xls(fn) %>% 
+        slice(-c(1:6))
+    colnames(dat) <- dat[1, ]
+    dat[-1, ]
+}
+
+course_dir <- "/Users/leisong/Library/CloudStorage/Dropbox/teaching/Rutgers/sda320"
+
+old_roster <- load_roster(file.path(cours_dir, "excelSectionReportAllSections.xls"))
+new_roster <- load_roster(file.path(cours_dir, "excelSectionReportAllSections_21_01.xls"))
+
+drop_students <- setdiff(old_roster, new_roster)
+add_students <- setdiff(new_roster, old_roster)
+
+drop_students$Student
+paste0(add_students$`Net ID`, "@scarletmail.rutgers.edu")
