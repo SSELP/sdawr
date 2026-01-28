4ede937d25a8a948de545a8ee022c77388c94b4a
Lei Song <pinot.sung@gmail.com>
2026-01-27 19:51:13 -0500

    Clean up slides folder

--- scripts/students.R
+++ scripts/students.R
@@ -10,11 +10,15 @@ load_roster <- function(fn){
 
 course_dir <- "/Users/leisong/Library/CloudStorage/Dropbox/teaching/Rutgers/sda320"
 
-old_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_21_01.xls"))
-new_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_23_01.xls"))
+old_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_23_01.xls"))
+new_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_26_01.xls"))
 
 drop_students <- setdiff(old_roster, new_roster)
 add_students <- setdiff(new_roster, old_roster)
 
 drop_students$Student
 paste0(add_students$`Net ID`, "@scarletmail.rutgers.edu")
+
+githubs <- c("am4403", "connorwoodcock", "noa20-pixel", "ananya-bongoni", 
+             "zennaahmed", "Lylatorr", "fm455", "djv93-svg", "divya-ananth", 
+             "tpcherry101", "stevenaraujov")
