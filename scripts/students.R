88cbdb52e47ae9edea379c92972423c9893f54ba
Lei Song <pinot.sung@gmail.com>
2026-01-31 18:35:58 -0500

    Auto-knit updates for site:

--- scripts/students.R
+++ scripts/students.R
@@ -10,8 +10,9 @@ load_roster <- function(fn){
 
 course_dir <- "/Users/leisong/Library/CloudStorage/Dropbox/teaching/Rutgers/sda320"
 
-old_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_23_01.xls"))
-new_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_26_01.xls"))
+# Update students during the add/drop period
+old_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_26_01.xls"))
+new_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_30_01.xls"))
 
 drop_students <- setdiff(old_roster, new_roster)
 add_students <- setdiff(new_roster, old_roster)
@@ -19,6 +20,20 @@ add_students <- setdiff(new_roster, old_roster)
 drop_students$Student
 paste0(add_students$`Net ID`, "@scarletmail.rutgers.edu")
 
-githubs <- c("am4403", "connorwoodcock", "noa20-pixel", "ananya-bongoni", 
-             "zennaahmed", "Lylatorr", "fm455", "djv93-svg", "divya-ananth", 
-             "tpcherry101", "stevenaraujov")
+# After add/drop period, update student's info
+githubs <- c("zennaahmed", "stevenaraujov", "noa20-pixel", "ananya-bongoni", 
+             "RohanDRU", "beg68", "DylanGong-1211", "", 
+             "tino1360", "", "vblopez10", "fm455", 
+             "", "am4403", "", "tpcherry101", 
+             "lylatorr", "connorwoodcock", "yyelrahc")
+
+repos <- c("za320", "SA320", "noa320", "asb320", 
+           "rd320", "bg320", "DG320", "", 
+           "sjl239", "", "", "", 
+           "", "ag320", "", "", 
+           "", "", "charley")
+
+students <- new_roster %>% select(Student, `Net ID`) %>% 
+    mutate(github = githubs, repo = repos)
+
+save(students, file = file.path(course_dir, "students.rda"))
