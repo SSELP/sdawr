eb8512697ae7275e4049aa13947445c2e74d7324
Lei Song <pinot.sung@gmail.com>
2026-02-07 23:40:41 -0500

    Auto-knit updates for site.

---
+++ scripts/assignments.R
@@ -0,0 +1,31 @@
+library(dplyr)
+
+# Define directories
+rutgers_dir <- "/Users/leisong/Library/CloudStorage/Dropbox/teaching/Rutgers"
+course_dir <- file.path(rutgers_dir, "sda320")
+ass_dir <- file.path(course_dir, "assignments")
+
+# Load students
+load(file.path(course_dir, "students.rda"))
+
+# Define some common strings
+sselp_url <- "git@github.com:SSELP/%s.git"
+personal_url <- "git@github.com:%s/%s.git"
+
+for (i in 1:nrow(students)){
+    stu <- students %>% slice(i)
+    message(sprintf("Student: %s, repo: %s", stu$Student, stu$repo))
+    
+    if (is.na(stu$sselp) | stu$github == "" | stu$repo == ""){
+        next()
+    }
+    
+    repo <- ifelse(stu$sselp == 1, sprintf(sselp_url, stu$repo),
+                   sprintf(personal_url, stu$github, stu$repo))
+    
+    if (dir.exists(file.path(ass_dir, stu$repo))){
+        system(sprintf("cd %s/%s; git pull", ass_dir, stu$repo))
+    } else{
+        system(sprintf("cd %s; git clone %s", ass_dir, repo))
+    }
+}
--- scripts/students.R
+++ scripts/students.R
@@ -8,7 +8,8 @@ load_roster <- function(fn){
     dat[-1, ]
 }
 
-course_dir <- "/Users/leisong/Library/CloudStorage/Dropbox/teaching/Rutgers/sda320"
+rutgers_dir <- "/Users/leisong/Library/CloudStorage/Dropbox/teaching/Rutgers"
+course_dir <- file.path(rutgers_dir, "sda320")
 
 # Update students during the add/drop period
 old_roster <- load_roster(file.path(course_dir, "excelSectionReportAllSections_26_01.xls"))
@@ -23,17 +24,23 @@ paste0(add_students$`Net ID`, "@scarletmail.rutgers.edu")
 # After add/drop period, update student's info
 githubs <- c("zennaahmed", "stevenaraujov", "noa20-pixel", "ananya-bongoni", 
              "RohanDRU", "beg68", "DylanGong-1211", "", 
-             "tino1360", "", "vblopez10", "fm455", 
-             "", "am4403", "", "tpcherry101", 
+             "tino1360", "ziadlehra", "vblopez10", "fm455", 
+             "", "am4403", "mcp245", "tpcherry101", 
              "lylatorr", "connorwoodcock", "yyelrahc")
 
 repos <- c("za320", "SA320", "noa320", "asb320", 
            "rd320", "bg320", "DG320", "", 
-           "sjl239", "", "vbl320", "fm320", 
-           "", "ag320", "", "tp320", 
+           "sjl239", "zl320", "vbl320", "fm320", 
+           "", "ag320", "mp320", "tp320", 
            "lt320", "cw320", "cy320")
 
+sselp <- c(1, 0, 0, 1,
+           1, 1, 0, NA,
+           1, 0, 0, 0,
+           NA, 0, 0, 0,
+           0, 1, 1)
+
 students <- new_roster %>% select(Student, `Net ID`) %>% 
-    mutate(github = githubs, repo = repos)
+    mutate(github = githubs, repo = repos, sselp = sselp)
 
 save(students, file = file.path(course_dir, "students.rda"))
