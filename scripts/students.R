9a6ff696933faaab779e48df2f653b85fa9e88d2
Lei Song <pinot.sung@gmail.com>
2026-02-15 19:33:17 -0500

    Auto-knit updates for site.

--- scripts/students.R
+++ scripts/students.R
@@ -23,19 +23,19 @@ paste0(add_students$`Net ID`, "@scarletmail.rutgers.edu")
 
 # After add/drop period, update student's info
 githubs <- c("zennaahmed", "stevenaraujov", "noa20-pixel", "ananya-bongoni", 
-             "RohanDRU", "beg68", "DylanGong-1211", "", 
+             "RohanDRU", "beg68", "DylanGong-1211", "rkk70", 
              "tino1360", "ziadlehra", "vblopez10", "fm455", 
              "", "am4403", "mcp245", "tpcherry101", 
              "lylatorr", "connorwoodcock", "yyelrahc")
 
 repos <- c("za320", "SA320", "noa320", "asb320", 
-           "rd320", "bg320", "DG320", "", 
+           "rd320", "bg320", "DG320", "myPackage", 
            "sjl239", "zl320", "vbl320", "fm320", 
            "", "ag320", "mp320", "tp320", 
            "lt320", "cw320", "cy320")
 
 sselp <- c(1, 0, 0, 1,
-           1, 1, 0, NA,
+           1, 1, 0, 0,
            1, 0, 0, 0,
            NA, 0, 0, 0,
            0, 1, 1)
