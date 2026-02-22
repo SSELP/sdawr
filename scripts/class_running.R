977f442fac2632193b4568fdfaf5eec1ee0fa81c
Lei Song <pinot.sung@gmail.com>
2026-02-22 12:05:01 -0500

    Auto-knit updates for site.

--- scripts/class_running.R
+++ scripts/class_running.R
@@ -2,6 +2,7 @@ library(rnaturalearth)
 library(dplyr)
 library(sf)
 library(ggplot2)
+library(terra)
 
 cnt_nms <- c("Latvia", "India", "Switzerland", "Pakistan", 
              "People's Republic of China", "Italy", 
@@ -56,3 +57,37 @@ dat <- cbind(dat, vars) %>%
     rename(latitude = decimalLatitude, longitude = decimalLongitude,
            temperature = bio01, precipitation = bio12)
 write.csv(dat, 'tropical_birds.csv', row.names = TRUE)
+
+# Get Spotted Hyenas data
+clean_keys <- name_backbone_checklist("Crocuta crocuta") %>% pull(usageKey)
+
+download_job <- occ_download(
+    pred_in("taxonKey", clean_keys),
+    pred("occurrenceStatus", "PRESENT"),
+    pred("hasCoordinate", TRUE),
+    pred("hasGeospatialIssue", FALSE),
+    pred_gte("distanceFromCentroidInMeters", 10), 
+    pred("taxonomicStatus", "ACCEPTED"), 
+    pred_gte("year", 1980), 
+    format = "SIMPLE_CSV")
+
+occ_download_get(download_job, path = ".", overwrite = TRUE)
+occurrence <- read.delim("0001734-260221153910048.csv") %>% 
+    dplyr::select(gbifID, species, countryCode, 
+                  decimalLatitude, decimalLongitude, 
+                  day, month, year) %>% 
+    filter(countryCode != "") %>% 
+    filter(!countryCode %in% c("US", "PH")) %>% 
+    na.omit() %>% distinct() %>% 
+    st_as_sf(coords = c('decimalLongitude', 'decimalLatitude'),
+             crs = 4326)
+
+bios <- rast(list.files("/Users/leisong/downloads/wc2", full.names = TRUE))
+variables <- extract(bios, occurrence, ID = FALSE, bind = TRUE) %>%
+    st_as_sf() %>% st_drop_geometry() %>% 
+    dplyr::select(c('gbifID', paste0('wc2.1_2.5m_bio_', 1:19)))
+write.csv(variables, 'docs/hyenas_variables.csv', row.names = FALSE)
+
+occurrence <- read.delim("0001734-260221153910048.csv") %>% 
+    filter(gbifID %in% variables$gbifID)
+write.csv(occurrence, 'docs/hyenas_occurrence.csv', row.names = FALSE)
